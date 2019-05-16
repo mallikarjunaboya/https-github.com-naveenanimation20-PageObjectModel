be076b926473e69b1d2943310e5d2fb6d7468332
mallikarjunaboya <arjunboya88@gmail.com>
2019-05-16 07:54:10 +0530

    ArjunReddy and Shiva pages are adde

---
+++ src/test/java/com/crm/qa/testcases/ArjunReddy.java
@@ -0,0 +1,10 @@
+package com.crm.qa.testcases;
+
+public class ArjunReddy {
+
+	public static void main(String[] args) {
+		System.out.println("Rashi");
+
+	}
+
+}
---
+++ src/test/java/com/crm/qa/testcases/Shiva.java
@@ -0,0 +1,10 @@
+package com.crm.qa.testcases;
+
+public class Shiva {
+
+	public static void main(String[] args) {
+		System.out.println("shiva");
+
+	}
+
+}
